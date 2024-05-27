ad53338dccb7185a20304a25b977c3644143d7d5
flanker-and-fulcrum <57795302+flanker-and-fulcrum@users.noreply.github.com>
2024-05-27 22:16:20 +0800

    Update TliasWebManagementApplication.java

--- tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
+++ tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
@@ -9,5 +9,6 @@ public class TliasWebManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(TliasWebManagementApplication.class, args);
         System.out.println("hello.txt");
+        System.out.println("hello222222.txt");
     }
 }
